7380c4dfa586edabe41b09f6a0c67e1028a0f83f
Anushka Agarwal <agarwalanushka4@gmail.com>
2024-12-24 19:43:36 +0530

    feat: implementing the logic for whitelisting and blacklisting wallets

--- contracts/ManagedToken.sol
+++ contracts/ManagedToken.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ManagedToken is ERC20, Ownable {
     mapping(address => bool) private whitelist;
+    mapping(address => address[]) private interactions;
 
     event WalletWhitelisted(address indexed wallet);
     event WalletBlacklisted(address indexed wallet);
@@ -21,6 +22,41 @@ contract ManagedToken is ERC20, Ownable {
         for (uint256 i = 0; i < initialWhitelist.length; i++) {
             whitelist[initialWhitelist[i]] = true;
             emit WalletWhitelisted(initialWhitelist[i]);
+        }
+    }
+
+    function isWhitelisted(address wallet) public view returns (bool) {
+        return whitelist[wallet];
+    }
+
+    function blacklistWallet(address wallet) external onlyOwner {
+        require(whitelist[wallet], "Wallet is already blacklisted");
+        _updateStateForInteractions(wallet, false);
+    }
+
+    function whitelistWallet(address wallet) external onlyOwner {
+        require(!whitelist[wallet], "Wallet is already whitelisted");
+        _updateStateForInteractions(wallet, true);
+    }
+
+    function _updateStateForInteractions(address wallet, bool state) private {
+        // Directly update the state of all wallets that have interacted with wallet
+        for (uint256 i = 0; i < interactions[wallet].length; i++) {
+            address peer = interactions[wallet][i];
+            whitelist[peer] = state;
+            if (state) {
+                emit WalletWhitelisted(peer);
+            } else {
+                emit WalletBlacklisted(peer);
+            }
+        }
+
+        // Update the state of the wallet itself
+        whitelist[wallet] = state;
+        if (state) {
+            emit WalletWhitelisted(wallet);
+        } else {
+            emit WalletBlacklisted(wallet);
         }
     }
 }
