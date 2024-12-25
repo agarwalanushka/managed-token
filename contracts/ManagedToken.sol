6bac40a12c865840626368b23aba0f30c107cd0b
Anushka Agarwal <agarwalanushka4@gmail.com>
2024-12-25 14:01:49 +0530

    feat: implemented the code for tranfer and interaction between wallets

--- contracts/ManagedToken.sol
+++ contracts/ManagedToken.sol
@@ -7,6 +7,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract ManagedToken is ERC20, Ownable {
     mapping(address => bool) private whitelist;
     mapping(address => address[]) private interactions;
+    mapping(address => mapping(address => bool)) private hasInteracted;
 
     event WalletWhitelisted(address indexed wallet);
     event WalletBlacklisted(address indexed wallet);
@@ -29,6 +30,19 @@ contract ManagedToken is ERC20, Ownable {
         return whitelist[wallet];
     }
 
+    function transfer(
+        address recipient,
+        uint256 amount
+    ) public override returns (bool) {
+        require(whitelist[msg.sender], "Sender is not whitelisted");
+        require(whitelist[recipient], "Recipient is not whitelisted");
+
+        // Record the interaction between sender and recipient
+        _recordInteraction(msg.sender, recipient);
+
+        return super.transfer(recipient, amount);
+    }
+
     function blacklistWallet(address wallet) external onlyOwner {
         require(whitelist[wallet], "Wallet is already blacklisted");
         _updateStateForInteractions(wallet, false);
@@ -37,6 +51,16 @@ contract ManagedToken is ERC20, Ownable {
     function whitelistWallet(address wallet) external onlyOwner {
         require(!whitelist[wallet], "Wallet is already whitelisted");
         _updateStateForInteractions(wallet, true);
+    }
+
+    function _recordInteraction(address wallet1, address wallet2) private {
+        if (!hasInteracted[wallet1][wallet2]) {
+            hasInteracted[wallet1][wallet2] = true;
+            hasInteracted[wallet2][wallet1] = true;
+
+            interactions[wallet1].push(wallet2);
+            interactions[wallet2].push(wallet1);
+        }
     }
 
     function _updateStateForInteractions(address wallet, bool state) private {
